35ad0da27218ffe848c9b4bbee38867cb8615f00
lenadroid <lenadroid@yandex.com>
2014-06-15 17:48:11 +0200

    Initial commit to LennyOps. Map without tests

---
+++ LennyOps/LennyOps.fs
@@ -0,0 +1,18 @@
+﻿module LennyOps
+
+type Ops =   
+    // map.
+    // Data Structures supported:
+    // List, Array, Array2D, Array3D, Seq, Map, Set
+    static member map (f: ('a -> 'b), (x: list<'a>)) = x |> List.map f
+    static member map (f: ('a -> 'b), (x: array<'a>)) = x |> Array.map f    
+    static member map (f: ('a -> 'b), (x: 'a[,])) = x |> Array2D.map f
+    static member map (f: ('a -> 'b), (x: 'a[,,])) = x |> Array3D.map f
+    static member map (f: ('a -> 'b), (x: seq<'a>)) = x |> Seq.map f
+    static member map (f: ('a -> 'b -> 'c), (x: Map<'a, 'b>)) = x |> Map.map f
+    static member map (f: ('a -> 'b), (x: Set<'a>)) = x |> Set.map f
+
+let inline mapOp< ^t, ^f, ^x, ^c when (^t or ^f) : (static member map : ^f * ^x -> ^c) > f x = 
+    ((^t or ^f) : (static member map : ^f * ^x -> ^c) (f, x))
+
+let inline map f x = mapOp<Ops, _, _, _> f x
---
+++ LennyOps/Program.fs
@@ -0,0 +1,17 @@
+﻿module LennyOpsTest
+
+open LennyOps
+
+[<EntryPoint>]
+let main argv = 
+
+    // declaration
+    let myArray = [|"fsharp";"fsharp";"fsharp";"fsharp";"fsharp"|]
+    let myList = ["fsharp";"fsharp";"fsharp";"fsharp";"fsharp"]
+    let mySequence =  seq {for i = 1 to 10 do yield "fsharp"}
+    let my2DArray = array2D [[1;2;3];[4;5;6];[7;8;9]]
+    let myMap = Map.ofList [ (1, "i"); (2, "love"); (3, "fsharp") ]
+    let mySet = Set.ofList ["I";"love";"fsharp";"very";"very";"much"] 
+
+    printfn "%A" argv
+    0 
